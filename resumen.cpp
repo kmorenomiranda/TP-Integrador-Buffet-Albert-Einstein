35d163487cbb879571fb52840806d851787cf2ee
Ayelen <alconayelenrom@gmail.com>
2026-09-23 23:31:50 -0300

    agrego int main

--- resumen.cpp
+++ resumen.cpp
@@ -15,7 +15,7 @@ void corteControl(const char* nombreArchivo) {
     FILE* f = fopen(nombreArchivo, "rb");
     if(f == NULL) { // Se activa cuando el usuario ejecuta resumen.cpp sin haber ejecutado cierre.cpp primero
         cout << "Error al intentar leer el archivo." << endl;
-        cout << "Asegúrese de haber ejecutado cierre.cpp primero." << endl;
+        cout << "Asegurese de haber ejecutado cierre.cpp primero." << endl;
         return;
     }
 
@@ -23,9 +23,9 @@ void corteControl(const char* nombreArchivo) {
     int leido = fread(&c, sizeof(Comanda), 1, f);
     int totalProductosBuffet = 0; // Para contar la cantidad de productos vendidos por el buffet en la semana 
 
-    cout << "........................................." << endl;
-    cout << "             RESUMEN SEMANAL             " << endl; // La pongo acá para que no se repita por cada mozo registrado
-    cout << "........................................." << endl;
+    cout << "............................................." << endl;
+    cout << "               RESUMEN SEMANAL               " << endl; // La pongo acá para que no se repita por cada mozo registrado
+    cout << "............................................." << endl;
 
     while(leido == 1) {
         int mozoActual = c.idMozo; // ID del mozo actual
@@ -38,12 +38,27 @@ void corteControl(const char* nombreArchivo) {
             totalProductosBuffet += c.cantidad; // -> buffet
             leido = fread(&c, sizeof(Comanda), 1, f);
         }
-
-        cout << "- Mozo ID" << mozoActual << " -" << endl;
+        
+        // El resumen del último mozo del cierre (que no tiene otro mozo después)
+        // Cuando se termina de leer todo el archivo, la funcion fread devuelve 0 y se sale del while, pero se muestran todos los mozos incluyendo el último
+        cout << "- Mozo ID " << mozoActual << " -" << endl;
         cout << "Productos vendidos: " << mozoProductos << endl;
         cout << "Comision: $" << mozoComision << endl;
+        cout << "_____________________________________________" << endl;
+
     }
+    cout << "- - - - - - - - - - - - - - - - - - - - - - -" << endl;
+    cout << "Total de productos vendidos por el buffet: " << totalProductosBuffet << endl; // Muestra el total vendido por el buffet
+    cout << "- - - - - - - - - - - - - - - - - - - - - - -" << endl;
+
     fclose(f);
 }
 
-// Agregar el total vendido por el buffet 
+// Como se van generando un comandas_semana_sX-mm.dat por cada semana, se le pide al usuario que ingrese el nombre del archivo semanal que desee ver en pantalla
+int main() {
+    char nombreArchivo[30];
+    cout << "Ingrese el nombre del archivo semanal (ej: comandas_semana_s1-06.dat): ";
+    cin >> nombreArchivo;
+    corteControl(nombreArchivo);
+    return 0;
+}
